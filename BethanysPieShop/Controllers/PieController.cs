096f4cae69d263cfecdf0b9a3be03a3825f6dd40
newmanuevers <seangnewman@msn.com>
2020-09-26 15:31:32 -0700

    Created contact page, partial views, components

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -23,19 +23,43 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-         public ViewResult List()
+        // public ViewResult List()
+        //{
+        //    // ViewBag.CurrentCategory = "Cheese Cakes";
+
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel
+        //    {
+        //        Pies = _pieRepository.AllPies,
+        //        CurrentCategory = "Cheese Cakes!"
+        //    };
+
+        //    return View(piesListViewModel);
+        //}
+        public ViewResult List(string category)
         {
-            // ViewBag.CurrentCategory = "Cheese Cakes";
+            IEnumerable<Pie> pies;
+            string currentCategory;
 
-            PiesListViewModel piesListViewModel = new PiesListViewModel
+            if (string.IsNullOrEmpty(category))
             {
-                Pies = _pieRepository.AllPies,
-                CurrentCategory = "Cheese Cakes!"
-            };
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                                                                        .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            }) ;
+            
 
-            return View(piesListViewModel);
+            
         }
-
         public IActionResult Details(int id)
         {
             var pie = _pieRepository.GetPieById(id);
